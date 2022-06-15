6167c899fc5d5ef338bc7618bdcb047b49415483
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2022-06-15 12:29:17 -0700

    implemented the RTS functions; added main method in RTS to act as a tester to ensure functions are working

--- interpreter/virtualmachine/RunTimeStack.java
+++ interpreter/virtualmachine/RunTimeStack.java
@@ -17,50 +17,127 @@ class RunTimeStack {
         framePointer.add(0);
     }
 
-//    public static void main(String[] args) {
-//        RunTimeStack rts = new RunTimeStack();
-//        rts.push(5);
-//        System.out.println(rts.peek());
-//        rts.push(6);
-//        System.out.println(rts.peek());
-//        System.out.println(rts.pop());
-//        System.out.println(rts.peek());
-//    }
+    /**
+     * This main function is used to test RunTimeStack methods to ensure that it is working as intended
+     */
+    public static void main(String[] args) {
+        RunTimeStack rts = new RunTimeStack();
+        System.out.println("**** TESTING push() and peek() ****");
+        rts.push(987654321);
+        System.out.println("Actual Result: " + rts.peek() + " | Expected: 987654321");
+        System.out.println("**** TESTING pop() ****");
+        System.out.println("Value returned by pop(): " + rts.pop() + " | Expected: 987654321");
+        System.out.println("Checking if RunTimeStack is empty(): " + rts.runTimeStack.isEmpty() + " | Expected: true");
+        System.out.println("**** TESTING dump() **** ");
+        System.out.print("Actual Result: ");
+        rts.push(1);
+        rts.push(2);
+        rts.push(3);
+        rts.newFrameAt(0);
+        rts.push(4);
+        rts.push(5);
+        rts.push(6);
+        rts.newFrameAt(0);
+        rts.push(7);
+        rts.push(8);
+        rts.newFrameAt(0);
+        rts.dump();
+        System.out.println(" | Expected: [1, 2, 3] [4, 5, 6] [7, 8]");
+        System.out.println("**** TESTING popFrame() ****");
+        System.out.print("BEFORE POP FRAME: ");
+        rts.framePointer.forEach(val -> System.out.print(val + " "));
+        rts.popFrame();
+        System.out.print("\nAFTER POP FRAME: ");
+        rts.framePointer.forEach(val -> System.out.print(val + " "));
+    }
 
     private int lastIndex() {
         return Math.max(this.runTimeStack.size() - 1, 0);
     }
 
+    /**
+     * Used for dumping the current state of the runTimeStack.
+     * It will print portions of the stack based on respective frame markers.
+     * Example: [1, 2, 3] [4, 5, 6] [7, 8]
+     * Frame Pointers would be 0, 3, 6
+     */
     public void dump() {
-
+        int lowerLimit = 0;
+        for(int i = 1; i < this.framePointer.size(); i++) {
+            int upperLimit = this.framePointer.get(i);
+            System.out.print(this.runTimeStack.subList(lowerLimit, upperLimit) + " ");
+            lowerLimit = upperLimit;
+        }
     }
 
+    /**
+     * returns the top of the runtime stack, but does not remove
+     * @return copy of the top of the stack
+     */
     public int peek() {
         return this.runTimeStack.get(lastIndex());
     }
+
+    /**
+     * Push the value to the top of the stack.
+     * @param value value to be pushed
+     * @return value pushed
+     */
     public int push(int value) {
         this.runTimeStack.add(value);
         return value;
     }
 
+    /**
+     * removes the top of the runtime stack
+     * @return the value popped.
+     */
     public int pop() {
         return this.runTimeStack.remove(lastIndex());
     }
 
+    /**
+     * Takes the top item of the run time stack, and stores it into an offset starting from the current frame.
+     * @param offsetFromFramePointer number of slots above current frame marker
+     * @return the item just stored
+     */
     public int store(int offsetFromFramePointer) {
-        return 0;
+        int slotsAboveCurrentFrameMarker = this.framePointer.peek() + offsetFromFramePointer;
+        int storedValue = this.pop();
+        return runTimeStack.set(slotsAboveCurrentFrameMarker, storedValue);
     }
 
+    /**
+     * Takes a value from the run time stack that is at the offset
+     * from the current frame marker and pushes it onto the top of the stack.
+     * @param offsetFromFramePointer number of slots above current frame marker
+     * @return item just loaded into the offset
+     */
     public int load(int offsetFromFramePointer) {
-        return 0;
+        int slotsAboveCurrentFrameMarker = this.framePointer.peek() + offsetFromFramePointer;
+        int loadValue = runTimeStack.get(slotsAboveCurrentFrameMarker);
+        return this.push(loadValue);
     }
 
+    /**
+     * create a new frame pointer at the index offset slots down from the top
+     * of the runtime stack.
+     * @param offsetFromTopOfRunStack slots down from the top of the runtime stack
+     */
     public void newFrameAt(int offsetFromTopOfRunStack) {
-
+        int slotsFromTopRTS = this.runTimeStack.size() - offsetFromTopOfRunStack;
+        this.framePointer.push(slotsFromTopRTS);
     }
 
+    /**
+     * pop the current frame off the runtime stack. Also removes the
+     * frame pointer value from the FramePointer Stack.
+     */
     public void popFrame() {
-
+        int wasCurr = this.framePointer.pop();
+        while(this.runTimeStack.size() > wasCurr) {
+            this.pop();
+        }
     }
 
 }
